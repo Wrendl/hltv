ddee56818e574f9153355f6fa4b83cb9a80c8f45
Nariman <60422821+Wrendl@users.noreply.github.com>
2023-12-08 13:43:26 +0600

    v1.6

--- front/src/components/data.js
+++ front/src/components/data.js
@@ -1,20 +1,23 @@
 export const data = {
     news : [
         {
+            id: 1,
             title: "ROPZ: \"I'M SUPER HAPPY TO BE PERFORMING LIKE THIS\"",
             text: "The Estonian rifler was heralded by teammate Twistzz as the \"best CS2 player\" shortly after FaZe's victory over MOUZ in the grand final.",
         },
         {
+            id: 2,
             title: "ROPZ NAMED CS ASIA CHAMPIONSHIPS 2023 MVP",
             text: "Robin \"ropz\" Kool has been named the Most Valuable Player (MVP) of the 2023 edition of the CS Asia Championships by HLTV and 1xBET after playing a crucial role in FaZe's title-winning campaign in Shanghai.",
         },
         {
+            id: 3,
             title: "FROZEN: \"I'M LOOKING FOR REVENGE OVER FAZE\"",
             text: "MOUZ are one step away from lifting their second big trophy in as many months after reaching the grand final of CS Asia Championships 2023 with a win over Astralis.",
         },
     ],
 
-    
+
     teams: [
         {
             id: 2,
@@ -66,6 +69,7 @@ export const data = {
 
     events : [
         {
+            id: 1,
             name: "BLAST Premier Spring Groups 2024",
             location: "Belgrade, Serbia",
             date: "Jan 22nd - Jan 28th",
